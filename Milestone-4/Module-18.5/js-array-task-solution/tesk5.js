e4ae8a44e501f77a35da1896f2210178835d976f
MukitHossen7 <hossenmukit7@gmail.com>
2024-08-31 15:59:01 +0600

    array task-4 is complate

---
+++ Milestone-4/Module-18.5/js-array-task-solution/tesk5.js
@@ -0,0 +1,8 @@
+//Task - 5
+
+const language1 = ["HTML", "CSS", "Tailwind"];
+const language2 = ["Javascript", "Go", "java", "Python"];
+const concatArray = language1.concat(language2);
+console.log(concatArray);
+console.log(language1);
+console.log(language2);
