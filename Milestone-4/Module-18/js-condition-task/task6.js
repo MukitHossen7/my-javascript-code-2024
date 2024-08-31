90ed71c5ef43bec69edfe2ebe4d7c8d3a982c739
MukitHossen7 <hossenmukit7@gmail.com>
2024-08-31 17:14:37 +0600

    task6 mathmatical related problem slove

--- Milestone-4/Module-18/js-condition-task/task6.js
+++ Milestone-4/Module-18/js-condition-task/task6.js
@@ -1,11 +1,11 @@
 //Task - 6
-
-let age = 30;
 let ticketPrice = 800;
+
+let age = 5;
 let isStudent = false;
 if (age < 10) {
   console.log("your ticket free");
-} else if (isStudent === true || (age > 10 && age < 28)) {
+} else if (isStudent) {
   let discount = (ticketPrice * 50) / 100;
   let discountBill = ticketPrice - discount;
   console.log(discountBill);
