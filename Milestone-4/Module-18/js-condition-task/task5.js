41bc881696156aa227035f0d3b86cf11d9c96ebd
MukitHossen7 <hossenmukit7@gmail.com>
2024-08-31 16:47:55 +0600

    task5 problem slove

--- Milestone-4/Module-18/js-condition-task/task5.js
+++ Milestone-4/Module-18/js-condition-task/task5.js
@@ -1,14 +1,14 @@
 // Task -5
 
-let num1 = 8;
+let num1 = 20;
 let num2 = 10;
 let result;
-// if (num1 > num2) {
-//   result = num1 ** 2;
-// } else {
-//   result = num1 + num2;
-// }
-// console.log(result);
+if (num1 > num2) {
+  result = num1 * 2;
+} else {
+  result = num1 + num2;
+}
+console.log(result);
 
-num1 > num2 ? (result = num1 ** 2) : (result = num1 + num2);
+num1 > num2 ? (result = num1 * 2) : (result = num1 + num2);
 console.log(result);
