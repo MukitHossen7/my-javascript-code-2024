165500a31ed01b544e2a85fc5e51e8aded7ae6e5
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-02 00:56:24 +0600

    for loop some problem practice

---
+++ Milestone-4/Module-19/class-19.4/for-loop.js
@@ -0,0 +1,29 @@
+//For Loop
+
+// for (let i = 0; i <= 25; i++) {
+//   console.log("The number is " + i);
+// }
+
+// for (let num = 20; num <= 100; num++) {
+//   console.log("The number is " + num);
+// }
+
+// for (let num = 0; num <= 20; num++) {
+//   if (num % 2 === 0) {
+//     console.log("Even number is " + num);
+//   }
+// }
+
+// for (let i = 2; i <= 20; i += 2) {
+//   console.log("The even number is " + i);
+// }
+
+// for (let i = 0; i <= 30; i++) {
+//   if (i % 2 !== 0) {
+//     console.log("The odd number is " + i);
+//   }
+// }
+
+for (let i = 1; i <= 30; i += 2) {
+  console.log("The odd number is " + i);
+}
