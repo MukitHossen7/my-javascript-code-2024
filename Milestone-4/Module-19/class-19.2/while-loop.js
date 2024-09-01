63b7f74cd0b8b00afb5537b46d29595b024e1167
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-02 00:20:14 +0600

    while loop example done

---
+++ Milestone-4/Module-19/class-19.2/while-loop.js
@@ -0,0 +1,53 @@
+//While Loop
+
+// let num = 0;
+// while (num <= 20) {
+//   console.log("The num is " + num);
+//   num++;
+// }
+
+// let num = 1;
+// while (num <= 10) {
+//   console.log("The num is " + num);
+//   num++;
+// }
+
+// let num = 50;
+// while (num <= 150) {
+//   console.log("The number is " + num);
+//   num++;
+// }
+
+// let num = 1;
+// let sum = 0;
+// while (num <= 10) {
+//   console.log(num);
+//   sum += num;
+//   num++;
+// }
+// console.log(sum);
+
+// let num = 10;
+// let sum = 0;
+// while (num <= 50) {
+//   console.log(num);
+//   sum += num;
+//   num++;
+// }
+// console.log(sum);
+
+// let num = 0;
+// while (num <= 20) {
+//   if (num % 2 === 0) {
+//     console.log("The even number is " + num);
+//   }
+//   num++;
+// }
+
+let num = 0;
+while (num <= 30) {
+  if (num % 2 !== 0) {
+    console.log("The odd number is " + num);
+  }
+  num++;
+}
