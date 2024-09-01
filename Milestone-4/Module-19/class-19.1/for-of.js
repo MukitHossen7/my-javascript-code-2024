79518c0383ac726295c6c5cf6d09e7b998e19584
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-01 23:33:46 +0600

    for of loop on array

---
+++ Milestone-4/Module-19/class-19.1/for-of.js
@@ -0,0 +1,15 @@
+//For of loop
+
+const numbers = [25, 30, 40, 50, 80, 60, 100, 90];
+console.log(numbers);
+
+for (let num of numbers) {
+  console.log(num);
+}
+
+const fruits = ["Apple ", "Mango", "Banana", "Orange", "Pinapple"];
+console.log(fruits);
+
+for (let fruit of fruits) {
+  console.log(fruit);
+}
