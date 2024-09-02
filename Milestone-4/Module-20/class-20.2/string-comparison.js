1796ed477e7183c7f13e514a7d70831118694f47
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-02 22:15:36 +0600

    string comparison practice done

---
+++ Milestone-4/Module-20/class-20.2/string-comparison.js
@@ -0,0 +1,22 @@
+// let inputEmail = "Mukit@gmaIl.coM";
+// let dataEmail = "mukit@gmail.com";
+
+// if (inputEmail.toLocaleLowerCase() === dataEmail.toLowerCase()) {
+//   console.log("You are admin");
+// } else {
+//   console.log("Your are Hacker");
+// }
+
+// let country = "Bangladesh";
+// let hobby = "programming";
+// console.log(country.toLowerCase());
+// console.log(hobby.toUpperCase());
+
+let inputName = "   mukit   ";
+let dataName = "mukit";
+
+if (inputName.toLocaleLowerCase().trim() === dataName.toLowerCase().trim()) {
+  console.log("You are admin");
+} else {
+  console.log("Your are Hacker");
+}
