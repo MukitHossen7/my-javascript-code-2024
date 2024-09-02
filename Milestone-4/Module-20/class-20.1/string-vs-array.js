c5e2c467bde24ed1af30a5db29d3c6599174c745
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-02 22:00:34 +0600

    string vs array

---
+++ Milestone-4/Module-20/class-20.1/string-vs-array.js
@@ -0,0 +1,17 @@
+let myName = "Mukit Hossen";
+
+let hobby = `Programming`;
+
+let numbers = [20, 30, 35, 40, 45, 50, 55];
+console.log(numbers);
+console.log(numbers.length);
+console.log(numbers[2]);
+numbers[1] = 100;
+console.log(numbers);
+
+let country = "Bangladesh";
+console.log(country);
+console.log(country.length);
+console.log(country[5]);
+country[0] = "I";
+console.log(country);
