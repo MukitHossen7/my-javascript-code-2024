6144f2c2d54fd1c5fba1e533118cd419329cfd76
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-02 16:25:23 +0600

    task 2 practice done

---
+++ Milestone-4/Module-19/js-loop-task-practice/for/task2.js
@@ -0,0 +1,17 @@
+// Task 2
+
+//Subtask-1:
+
+for (let num = 61; num <= 100; num++) {
+  if (num % 2 === 0) {
+    console.log("odd number is " + num);
+  }
+}
+
+// Subtask-2:
+
+for (let num = 78; num <= 98; num++) {
+  if (num % 2 === 1) {
+    console.log("even number is " + num);
+  }
+}
