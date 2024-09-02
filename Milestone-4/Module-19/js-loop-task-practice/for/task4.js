de6cd4a20262dae27039018ab8ccaa845d1ad41e
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-02 17:01:35 +0600

    task 4 practice complete

---
+++ Milestone-4/Module-19/js-loop-task-practice/for/task4.js
@@ -0,0 +1,13 @@
+// Task 4
+
+// let mul;
+// for (let num = 1; num <= 10; num++) {
+//   if ((mul = num * 9)) {
+//     console.log(mul);
+//   }
+// }
+
+let number = 9;
+for (let num = 1; num <= 10; num++) {
+  console.log(`${number} x ${num} = ${number * num}`);
+}
