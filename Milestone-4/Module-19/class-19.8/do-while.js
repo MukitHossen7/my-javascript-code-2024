af60a08d86b2bcc820613fb8b16d36a77e3cbd15
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-02 14:49:05 +0600

    do while loop example done

---
+++ Milestone-4/Module-19/class-19.8/do-while.js
@@ -0,0 +1,17 @@
+// let num = 1;
+// while (num <= 10) {
+//   console.log("The number is " + num);
+//   num++;
+// }
+
+let num = 1;
+while (num > 10) {
+  console.log("The number is " + num);
+  num++;
+}
+
+let i = 1;
+do {
+  console.log("This is do while " + i);
+  i++;
+} while (i > 10);
