cb57cfb8a4eadd87143e2cf2c108c04cce2ae706
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-02 14:33:20 +0600

    break-continue example done

---
+++ Milestone-4/Module-19/class-19.7/break-continue.js
@@ -0,0 +1,20 @@
+// let num = 1;
+// while (num <= 40) {
+//   console.log("The number is " + num);
+//   if (num >= 25) {
+//     break;
+//   }
+
+//   num++;
+// }
+
+for (let i = 10; i <= 50; i++) {
+  if (i === 15) {
+    continue;
+  } else if (i === 20) {
+    continue;
+  } else if (i === 25) {
+    continue;
+  }
+  console.log("The num is " + i);
+}
