1e09b5d5d5c110c42baf003c2a8bcc0bf4de9e17
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-02 20:33:37 +0600

    tesk 1 done

--- Milestone-4/Module-19/js-loop-task-practice/for/task1.js
+++ Milestone-4/Module-19/js-loop-task-practice/for/task1.js
@@ -1,5 +1,6 @@
 //Task 1;
 let message = "I will invest at least 6 hrs every single day for next 60 days!";
 for (let i = 1; i <= 60; i++) {
-  console.log(message);
+  // console.log(message + " " + i);
+  console.log(`${message} ${i}`);
 }
