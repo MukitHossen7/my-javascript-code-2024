2faaeca9b9f0c6524b790d5637fa9b2e2ad223b3
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-02 10:57:54 +0600

    for loop example done

---
+++ Milestone-4/Module-19/class-19.5/for-loop-example.js
@@ -0,0 +1,17 @@
+// let num = 10;
+// let sum = 0;
+
+// for (let num = 10; num <= 30; num++) {
+//   sum += num;
+//   console.log("The sum is " + sum);
+//   console.log(num);
+// }
+// console.log("Total :" + sum);
+
+// for (let i = 70; i <= 200; i++) {
+//   console.log("The num is " + i);
+// }
+
+for (let num = 50; num >= 10; num--) {
+  console.log(num);
+}
