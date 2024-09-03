fae79cf7a675e6778e46f4eb37c02912543e918f
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-03 10:33:27 +0600

    object practice comlate

---
+++ Milestone-4/Module-20/class-20.5/object.js
@@ -0,0 +1,25 @@
+const persons = {
+  name: "Mukit",
+  age: 24,
+  isMaried: false,
+  hobby: "Programming",
+};
+
+console.log(persons);
+
+const cars = {
+  name: "BMW",
+  model: 500,
+  isNew: true,
+  color: "White",
+  favCar: ["Honda", "Toyota", "TATA"],
+  run: function () {
+    console.log("This is high power car");
+  },
+  weight: {
+    min: "400kg",
+    normal: "600kg",
+    max: "1000kg",
+  },
+};
+console.log(cars);
