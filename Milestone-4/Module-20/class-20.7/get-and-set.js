34c6d8a0dbb0cd338771ccadb0e1b86a7e286035
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-03 10:55:24 +0600

    get and set using object practice complate

---
+++ Milestone-4/Module-20/class-20.7/get-and-set.js
@@ -0,0 +1,14 @@
+const person = {
+  name: "Mukit Hossen",
+  age: 24,
+  hobby: "Programming",
+  "gf name": ["Toma", "joma", "khabona"],
+};
+person.isMarid = true;
+person["nationlity"] = "Bangladeshi";
+person.age = 30;
+person["name"] = "Ali Hassan";
+console.log(person);
+// console.log(person.name);
+// console.log(person["hobby"]);
+// console.log(person["gf name"]);
