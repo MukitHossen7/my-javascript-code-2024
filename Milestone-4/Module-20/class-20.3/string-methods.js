1034b1c3509a284900b74941b92d5ba52a10164a
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-03 07:42:43 +0600

    string method practice done

---
+++ Milestone-4/Module-20/class-20.3/string-methods.js
@@ -0,0 +1,34 @@
+// let text = "Bangladesh";
+// console.log(text);
+// console.log(text.slice(2, 6));
+// console.log(text.slice(6, 10));
+// console.log(text.substring(1, 3));
+// console.log(text.substr(3, 4));
+
+// let text = "How are you do ing today?";
+// console.log(text);
+
+// let newText = text.split(" ", 3);
+// let newText = text.split("o");
+// console.log(newText);
+
+// let newText = text.split("").join("");
+// console.log(newText);
+
+// let text1 = "Hello";
+// let text2 = "I am a programmer";
+// let text3 = "I love programming";
+// let newText = text1.concat(" ").concat(text2, " ", text3);
+// console.log(newText);
+
+// let text1 = "Hello";
+// let text2 = "I am a programmer";
+// let text3 = "I love programming";
+
+// let newText = text1 + " " + text2 + " " + text3;
+// console.log(newText);
+
+let text = "Hello world, welcome to the universe.";
+console.log(text);
+let isIncludes = text.includes("programmer");
+console.log(isIncludes);
