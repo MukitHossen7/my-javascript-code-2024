f0912e49037f050a2cf0c416af111ed7fbf6b95d
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-03 16:50:13 +0600

    task 4 practice done

---
+++ Milestone-4/Module-20/js-object-solution/task4.js
@@ -0,0 +1,12 @@
+let student = {
+  name: "Ariana Grande",
+  age: 21,
+  city: "Gaibandha",
+  isStudent: true,
+};
+
+// console.log(student);
+let keys = Object.keys(student);
+// console.log(keys);
+let keysLength = keys.length;
+console.log(keysLength);
