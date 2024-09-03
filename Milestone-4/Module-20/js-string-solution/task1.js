52a9b6608f48338a75577a170fd5313b4ee3c308
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-03 19:41:06 +0600

    task 1 practice complete

---
+++ Milestone-4/Module-20/js-string-solution/task1.js
@@ -0,0 +1,12 @@
+let text = "Jhankar Mahbub";
+console.log(text);
+
+// for (let newText of text) {
+//   console.log(newText);
+//   let text2 = newText.search("a");
+
+//   console.log(text2);
+// }
+
+let ss = text.split("");
+console.log(ss);
