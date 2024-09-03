a0021bef596a62af5d834f7595ea7f6f65871b35
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-03 16:20:57 +0600

    task 3 practice complete

---
+++ Milestone-4/Module-20/js-object-solution/task3.js
@@ -0,0 +1,13 @@
+//Task 3
+
+const student = {
+  name: "Hamim Sakep",
+  id: 5421,
+  physics: {
+    subject: "HSC Physics",
+    author: "Shahjahan Tapan",
+    marks: 30,
+  },
+};
+
+console.log(student.physics.marks);
