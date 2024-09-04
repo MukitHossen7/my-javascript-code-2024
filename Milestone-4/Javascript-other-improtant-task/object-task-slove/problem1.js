e02738f6cc382d2440290d53c166c22f42f6ea8a
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-04 17:02:32 +0600

    slove problem 1

---
+++ Milestone-4/Javascript-other-improtant-task/object-task-slove/problem1.js
@@ -0,0 +1,9 @@
+const person = {
+  name: "Mukit Hossen",
+  age: 24,
+  city: "Naogaon",
+};
+// console.log(person);
+console.log(person.name);
+console.log(person.age);
+console.log(person.city);
