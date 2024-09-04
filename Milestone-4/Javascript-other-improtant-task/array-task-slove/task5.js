061daa50285f9e2ecd0d72a31f11e1a318d42e0f
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-04 14:41:43 +0600

    task 5 problem slove

---
+++ Milestone-4/Javascript-other-improtant-task/array-task-slove/task5.js
@@ -0,0 +1,10 @@
+const names = ["Mukit", "Toma", "Hiya", "Titly", "Runju"];
+console.log(names);
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
+
+for (let name of names) {
+  console.log(name);
+}
