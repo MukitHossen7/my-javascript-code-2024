21adf6ddfd805f776461dde4dd721020294b6350
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-04 15:11:44 +0600

    task 7 problem slove

---
+++ Milestone-4/Javascript-other-improtant-task/array-task-slove/task7.js
@@ -0,0 +1,7 @@
+const numbers = [15, 50, 45, 25, 80, 65, 70, 35, 100];
+console.log(numbers);
+console.log(numbers.indexOf(5));
+
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(number);
+console.log(number.indexOf(5));
