a3f5c1a78e0c9a1f565ecd3751d993dcf93c3e9a
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-04 11:15:07 +0600

    task 3 practice done

---
+++ Milestone-4/Module-20.5/array-looping-task-solution/task3.js
@@ -0,0 +1,12 @@
+// ### Task 3
+
+const texts = ["Tom", "Tim", "Tin", "Tik"];
+console.log(texts);
+
+let stringText = "";
+for (let text of texts) {
+  stringText += text;
+}
+// console.log(`'${stringText}'`);
+console.log(stringText);
+console.log(typeof stringText);
