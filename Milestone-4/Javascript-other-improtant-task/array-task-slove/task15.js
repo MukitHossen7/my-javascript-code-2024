f32c185d101fda0c9a6de3e35593fb7bb0284d74
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-04 21:13:08 +0600

    task 15 problem solve

--- Milestone-4/Javascript-other-improtant-task/array-task-slove/task15.js
+++ Milestone-4/Javascript-other-improtant-task/array-task-slove/task15.js
@@ -1,7 +1,6 @@
 const numbers = [15, 30, 24, 35, 40, 80, 70, 65, 60, 5];
-// let maxNumber = numbers.max();
-// console.log(maxNumber);
-for (let number of numbers) {
-  console.log(number);
-  console.log(max(number));
-}
+
+console.log(numbers);
+let num = Math.max(...numbers);
+console.log(...numbers);
+console.log(num);
