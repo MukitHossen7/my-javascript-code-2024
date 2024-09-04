5fb853604fb1b668c245bcb63a9cbdbdb391c7bb
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-04 16:44:34 +0600

    task 12 problem slove

---
+++ Milestone-4/Javascript-other-improtant-task/array-task-slove/task12.js
@@ -0,0 +1,3 @@
+const integersNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15];
+const newArray = integersNum.slice(0, 3);
+console.log(newArray);
---
+++ Milestone-4/Javascript-other-improtant-task/array-task-slove/task15.js
@@ -0,0 +1,7 @@
+const numbers = [15, 30, 24, 35, 40, 80, 70, 65, 60, 5];
+// let maxNumber = numbers.max();
+// console.log(maxNumber);
+for (let number of numbers) {
+  console.log(number);
+  console.log(max(number));
+}
