03f56b5a17bb97f7d941a4e16ee79e1d3229eb9d
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-04 15:19:06 +0600

    task 8 problem slove

---
+++ Milestone-4/Javascript-other-improtant-task/array-task-slove/task8.js
@@ -0,0 +1,12 @@
+const fruits = [
+  "Banana",
+  "Mangp",
+  "Apple",
+  "Pinapple",
+  "Jackfruites",
+  "Orange",
+];
+// console.log(fruits);
+
+let sortArray = fruits.sort();
+console.log(sortArray);
