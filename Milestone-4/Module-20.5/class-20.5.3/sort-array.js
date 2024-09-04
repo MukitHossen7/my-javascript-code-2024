4d2c668e8efc07c6b58f8aabe440bb806c0671a8
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-04 09:27:03 +0600

    sort array practice done

---
+++ Milestone-4/Module-20.5/class-20.5.3/sort-array.js
@@ -0,0 +1,16 @@
+const friends = ["Elon", "Bill", "Mark", "Jobes", "Ambani"];
+console.log(friends);
+
+const arraySort = friends.sort();
+console.log(arraySort);
+
+// const num = [2, 8, 9, 5, 1, 7];
+// console.log(num);
+
+// const sortNum = num.sort();
+// console.log(sortNum);
+
+const num = [10, 15, 11, 20, 29, 22, 35, 80, 50, 1, 100];
+console.log(num);
+const numSort = num.sort();
+console.log(numSort);
