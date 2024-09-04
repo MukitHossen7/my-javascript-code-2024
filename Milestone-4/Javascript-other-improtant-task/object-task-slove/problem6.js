b2fe7ebb157ba420512fbba5ca053795bf4f049c
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-04 22:11:13 +0600

    problem 6 slove

--- Milestone-4/Javascript-other-improtant-task/object-task-slove/problem6.js
+++ Milestone-4/Javascript-other-improtant-task/object-task-slove/problem6.js
@@ -3,7 +3,18 @@
 Objective: Given two objects, merge them into one new object and display the result.
 */
 
-const persons = {
-  firstName: "Mukit",
-  lastName: "Hossen",
+let person1 = {
+  firstName1: "John",
+  lastName1: "Doe",
+  age: 50,
+  eyeColor: "blue",
 };
+const person2 = { firstName: "Anne", lastName: "Smith" };
+
+let person = {
+  ...person1,
+  ...person2,
+};
+console.log(person);
+
+console.log(Object.assign(person1, person2));
