0fde048eb06959a40bbc29786d627091e23e220a
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-04 16:10:20 +0600

    task 14 problem slove

---
+++ Milestone-4/Javascript-other-improtant-task/array-task-slove/task14.js
@@ -0,0 +1,7 @@
+const words = ["B", "a", "n", "g", "l", "a", "d", "e", "s", "h"];
+let newWords = words.join("");
+console.log(newWords);
+
+const words2 = ["Hello", "world", "this", "is", "javascript"];
+let stringWord = words2.join(" ");
+console.log(stringWord);
