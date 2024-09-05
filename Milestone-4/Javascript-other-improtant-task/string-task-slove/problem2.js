29fbf0046d95be4bb53b44819bb0cd528a97f818
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-05 07:55:09 +0600

    problem 2 slove

---
+++ Milestone-4/Javascript-other-improtant-task/string-task-slove/problem2.js
@@ -0,0 +1,10 @@
+/*
+2. Problem: Merge Two Arrays
+    Objective: Merge two arrays into one using the concat() method.
+*/
+
+let text1 = "Hi! I am Mukit";
+let text2 = "I am a Programmer";
+
+let mergeArray = text1.concat(" ").concat(text2);
+console.log(mergeArray);
