e246a1ee77fb8d67751118dcf8ae8415fef88259
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-05 22:47:18 +0600

    parameters and arguments function practice done

---
+++ Milestone-4/Module-21/class-21.3,4/para-and-function.js
@@ -0,0 +1,38 @@
+function sum(num1, num2) {
+  let sum = num1 + num2;
+  console.log(sum);
+}
+sum(50, 60);
+sum(150, 60);
+sum(500, 60);
+
+console.log("================");
+function multipication(num1, num2) {
+  let mult = num1 * num2;
+  console.log(mult);
+}
+multipication(10, 5);
+multipication(20, 5);
+
+console.log("=================");
+
+// function multipication(num1, num2) {
+//   let mult = num1 * num2;
+//   console.log(mult);
+// }
+
+// let num1 = 20;
+// let num2 = 10;
+// multipication(num1, num2);
+
+console.log("===================");
+
+function sumAll(num1, num2, num3, num4) {
+  let sum = num1 + num2 + num3 + num4;
+  console.log(sum);
+}
+let num1 = 20;
+let num2 = 10;
+let num3 = 50;
+let num4 = 30;
+sumAll(num1, num2, num3, num4);
