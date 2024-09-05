d8ce211fc53002e57d2cca2bbc874c15b6b092dd
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-05 19:29:27 +0600

    task 5 problem slove

--- Milestone-4/Module-20/js-string-solution/taxt5.js
+++ Milestone-4/Module-20/js-string-solution/taxt5.js
@@ -3,20 +3,11 @@
 Capitalize Every first Letter of each word in a String
  */
 
-const mySentence = "freeCodeCamp is an awesome resource";
-const words = mySentence.split(" ");
+let sentences = "hello i am programmer mukit";
+let words = sentences.split(" ");
 
 for (let i = 0; i < words.length; i++) {
-  words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+  words[i] = words[i][0].toUpperCase().concat(words[i].slice(1));
 }
-
-console.log(words.join(" "));
-
-// let sentences = "hello i am programmer mukit";
-// let words = sentences.split(" ");
-
-// for (let i = 0; i < sentences.length; i++) {
-//   // words[i] = words[i][0].toUpperCase() + words[i].substr(1) + " ";
-//   words[i] = words[i][0].toUpperCase() + words[i].substr(1) + " ";
-// }
-// console.log(words);
+let result = words.join(" ");
+console.log(result);
