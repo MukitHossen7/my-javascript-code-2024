eefa99dfb0c667d7727eab394e9933e3f4b0f057
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-05 23:17:22 +0600

    return function practice done

---
+++ Milestone-4/Module-21/class-21.5/return-function.js
@@ -0,0 +1,19 @@
+function multipication(num1, num2) {
+  let mult = num1 * num2;
+  return mult;
+}
+let num1 = 20;
+let num2 = 10;
+let result = multipication(num1, num2);
+console.log(result);
+
+console.log("================");
+
+function myName(fName, lName) {
+  let fullName = fName + " " + lName;
+  return fullName;
+}
+let fName = "Mukit";
+let lName = "Hossen";
+let fullName = myName(fName, lName);
+console.log(fullName);
