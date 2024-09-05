9f90d8d0391d86094ed88a49a2ed89d459b18bb1
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-05 22:13:11 +0600

    intro function

---
+++ Milestone-4/Module-21/class-21.1/intro-function.js
@@ -0,0 +1,12 @@
+function myLearning() {
+  console.log(`HTML`);
+  console.log(`CSS`);
+  console.log(`Javascript`);
+  console.log(`Tailwind`);
+}
+
+myLearning();
+myLearning();
+myLearning();
+myLearning();
+myLearning();
