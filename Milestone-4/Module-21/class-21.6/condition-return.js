b1f8010e56fe4c848e6fe71d0911aa1eeb27c113
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-06 00:06:02 +0600

    return condition practice task is complaite

---
+++ Milestone-4/Module-21/class-21.6/condition-return.js
@@ -0,0 +1,31 @@
+// function product(price1, price2) {
+//   return price1 + price2;
+// }
+
+// let price1 = 500;
+// let price2 = 300;
+
+// // let result = product(price1, price2);
+// // console.log(result);
+
+// console.log(product(price1, price2));
+
+// function isEven(num) {
+//   if (num % 2 === 0) {
+//     return "This is even number";
+//   } else {
+//     return "This is odd number";
+//   }
+// }
+
+// let number = 9;
+// console.log(isEven(number));
+
+function isOdd(num) {
+  if (num % 2 === 1) {
+    return "This is odd number";
+  }
+  return "This is even number";
+}
+let number = 40;
+console.log(isOdd(number));
