f5678ec6a8ca7bafd0a9457704f90442136ffa1a
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-06 11:38:24 +0600

    problem one is slove

---
+++ Milestone-4/Module-21/js-function-solution/problem1.js
@@ -0,0 +1,15 @@
+/*
+### Task-1  
+Take four parameters. Multiply the four numbers and then return the result 
+ */
+
+function multiplyNums(num1, num2, num3, num4) {
+  return num1 * num2 * num3 * num4;
+}
+
+let num1 = 5;
+let num2 = 2;
+let num3 = 3;
+let num4 = 10;
+let result = multiplyNums(num1, num2, num3, num4);
+console.log(result);
