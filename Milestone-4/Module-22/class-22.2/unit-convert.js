e7eb3f32957cce03e6da4a47745cdd3a1b1c697b
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-06 23:05:34 +0600

    unit convert prastice is complete

---
+++ Milestone-4/Module-22/class-22.2/unit-convert.js
@@ -0,0 +1,29 @@
+//Inch to Feet
+
+// function inchToFeet(inch) {
+//   let feet = inch * 0.0833333;
+//   return feet.toFixed(1) + " feet";
+// }
+// console.log(inchToFeet(68.4));
+
+//Feet to inch
+// function feetToInch(feet) {
+//   let inch = feet * 12;
+//   return inch.toFixed(2);
+// }
+// console.log(feetToInch(5.7));
+
+//Miles to Kilometers
+// function milesToKilometers(miles) {
+//   let kilo = miles * 1.60934;
+//   return kilo.toFixed(2) + " kilometers";
+// }
+// console.log(milesToKilometers(7));
+
+//Kilometers to Miles
+
+function kilometersToMiles(kilometers) {
+  let miles = kilometers * 0.621371;
+  return miles.toFixed(2) + " miles";
+}
+console.log(kilometersToMiles(100));
