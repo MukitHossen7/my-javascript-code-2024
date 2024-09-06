db34145d1e90423e2fa0c06413945f8591b74208
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-06 16:25:02 +0600

    problem 1 practice

---
+++ Milestone-4/Javascript-other-improtant-task/array-and-object-problem-slove/problem1.js
@@ -0,0 +1,9 @@
+const students = [
+  { name: "Mukit", age: 24, grade: "A+" },
+  { name: "Raju", age: 26, grade: "A" },
+  { name: "Ranju", age: 21, grade: "A+" },
+];
+console.log(students);
+console.log(students[1].name);
+console.log(students[2]);
+console.log(students[2].grade);
