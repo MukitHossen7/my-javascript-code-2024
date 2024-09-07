8da3f004a93ce9850b7bfa56bd5468dc105f1f4d
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-07 22:39:43 +0600

    max numbers is complate

---
+++ Milestone-4/Module-23/class-23.1/maxNumbers.js
@@ -0,0 +1,38 @@
+// let sadiya = 95;
+// let mariya = 100;
+
+// function getMax(num1, num2) {
+//   if (num1 > num2) {
+//     return "You get maxNumber " + num1;
+//   }
+//   return "You get maxNumber " + num2;
+// }
+// console.log(getMax(sadiya, mariya));
+
+// function maxNumber(num1, num2) {
+//   if (num1 > num2) {
+//     return num1;
+//   }
+//   return num2;
+// }
+// let num1 = Math.max(40, 50);
+// let num2 = Math.max(62, 60);
+// console.log(maxNumber(num1, num2));
+
+// function getMaxNumber(num1, num2, num3) {
+//   if (num1 > num2 && num1 > num3) {
+//     return "tom get highest Number";
+//   } else if (num2 > num1 && num2 > num3) {
+//     return "tim get highest Number";
+//   } else {
+//     return "tik get highest Number";
+//   }
+// }
+
+// let tom = 85;
+// let tim = 90;
+// let tik = 128;
+// console.log(getMaxNumber(tom, tim, tik));
+
+let maxNumber = Math.max(10, 25, 45, 80, 74, 5);
+console.log(maxNumber);
