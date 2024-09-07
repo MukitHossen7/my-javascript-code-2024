d7a9ebf34c93213cd830bb78777024d4d6ecedd7
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-07 12:13:12 +0600

    swap practice done

---
+++ Milestone-4/Module-22/class-22.8/swap.js
@@ -0,0 +1,50 @@
+// Example :1
+
+// let a = 5;
+// let b = 7;
+// console.log(a, b);
+
+// // a = b;
+// // b = a;
+// // console.log(a, b);
+
+// let swap = a;
+// a = b;
+// b = swap;
+// console.log(a, b);
+
+//Example 2
+
+// let a = 20;
+// let b = 100;
+// console.log(a, b);
+
+// // a = b;
+// // b = a;
+// // console.log(a, b);
+
+// let swap = a;
+// a = b;
+// b = swap;
+// console.log(a, b);
+
+// Example 3
+
+// let a = 20;
+// let b = 100;
+// console.log(a, b);
+
+// // a = b;
+// // b = a;
+// // console.log(a, b);
+
+// [a, b] = [b, a];
+// console.log(a, b);
+
+// Example -4
+
+let x = "toma";
+let y = "hiya";
+console.log(x, y);
+[x, y] = [y, x];
+console.log(x, y);
