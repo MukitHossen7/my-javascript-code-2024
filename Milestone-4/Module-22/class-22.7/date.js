423005aba808231b0cfa785dc56a4a2afedd0c72
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-07 10:08:07 +0600

    date related some practice

---
+++ Milestone-4/Module-22/class-22.7/date.js
@@ -0,0 +1,13 @@
+// let date = new Date();
+// console.log(date);
+
+let date = new Date("2050-05-25");
+console.log(date);
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getMonth());
+console.log(date.getFullYear());
+console.log(date.getHours());
+console.log(date.getMilliseconds());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
