881583c9863ad7ca3981f8c68d7e5f920fbda648
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-09 22:16:32 +0600

    problem 4 is complete

---
+++ Milestone-4/B9-Javascript-assignment-slove/problem4.js
@@ -0,0 +1,21 @@
+function password(obj) {
+  if (
+    obj.name === undefined ||
+    obj.birthYear === undefined ||
+    obj.siteName === undefined ||
+    obj.birthYear.toString().length !== 4
+  ) {
+    return "invalid";
+  }
+  let siteName = obj.siteName.charAt(0).toUpperCase() + obj.siteName.slice(1);
+  let password = siteName + "#" + obj.name + "@" + obj.birthYear;
+  return password;
+}
+//Test
+
+console.log(
+  password({ name: "kolimuddin", birthYear: 1999, siteName: "google" })
+);
+console.log(password({ name: "rahat", birthYear: 2002, siteName: "Facebook" }));
+console.log(password({ name: "toky", birthYear: 200, siteName: "Facebook" }));
+console.log(password({ name: "maisha", birthYear: 2002 }));
