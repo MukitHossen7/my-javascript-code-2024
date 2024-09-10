7ae231cc0efbf13598f60020840bb78af6fcc46e
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-10 23:19:47 +0600

    problem 2 done

---
+++ Milestone-4/B10-javascript-assignment-slove/problem2.js
@@ -0,0 +1,17 @@
+function sendNotification(email) {
+  if (!email.includes("@")) {
+    return "Invalid Email";
+  }
+  let mail = email.split("");
+  let length = mail.length;
+  let index = mail.indexOf("@");
+  let userName = mail.slice(0, index).join("");
+  let domainName = mail.slice(index + 1, length).join("");
+  let message = `${userName} sent you an email from ${domainName}`;
+  return message;
+}
+console.log(sendNotification("zihad@gmail.com"));
+console.log(sendNotification("farhan34@yahoo.com"));
+console.log(sendNotification("nadim.naem5@outlook.com"));
+console.log(sendNotification("fahim234.hotmail.com"));
+console.log(sendNotification("fahim234.hotmail.com"));
