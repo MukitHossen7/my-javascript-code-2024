712509df7a1b4956c80b77b4541833a66427576c
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-10 11:48:22 +0600

    problem 5 is slove

--- Milestone-4/B9-Javascript-assignment-slove/problem5.js
+++ Milestone-4/B9-Javascript-assignment-slove/problem5.js
@@ -1,6 +1,26 @@
-function monthlySavings() {}
-// input array = [500,200,1000,3050,2000]
-// living cost = 5000
+function monthlySavings(arr, livingCost) {
+  if (!Array.isArray(arr) || typeof livingCost !== "number") {
+    return "invalid input";
+  }
+  let totalPayment = 0;
+  for (let payment of arr) {
+    if (payment >= 3000) {
+      let tex = payment * 0.2;
+      totalPayment += payment - tex;
+    } else {
+      totalPayment += payment;
+    }
+  }
+  let savings = totalPayment - livingCost;
+  if (savings >= 0) {
+    return savings;
+  } else {
+    return "earn more";
+  }
+}
 
-let tax = 3000 * 0.2;
-console.log(tax);
+console.log(monthlySavings([1000, 2000, 3000], 5400));
+console.log(monthlySavings([1000, 2000, 2500], 5000));
+console.log(monthlySavings([900, 2700, 3400], 10000));
+console.log(monthlySavings(100, [900, 2700, 3400]));
+console.log(monthlySavings({ name: "mukit", price: 20 }, 100));
