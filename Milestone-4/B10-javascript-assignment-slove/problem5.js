1d2199a1db91ed3425fc68330e8a465b60d20c6f
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-11 12:12:24 +0600

    problem 5 is slove

--- Milestone-4/B10-javascript-assignment-slove/problem5.js
+++ Milestone-4/B10-javascript-assignment-slove/problem5.js
@@ -4,15 +4,21 @@ function waitingTime(waitingTimes, serialNumber) {
   }
   let length = waitingTimes.length;
   let totalTime = 0;
-  console.log(length);
   for (let times of waitingTimes) {
-    console.log(times);
     totalTime += times;
   }
-  console.log(totalTime);
+  let averageInterviewTime = Math.round(totalTime / length);
+  let serialNo = serialNumber - 1;
+  let interviewPending = serialNo - length;
+  //   let isratWaitingTime = averageInterviewTime * interviewPending;
+  let isratWaitingTime =
+    interviewPending > 0 ? averageInterviewTime * interviewPending : 0;
+  return isratWaitingTime;
 }
 console.log(waitingTime([3, 5, 7, 11, 6], 10));
-// console.log(waitingTime(7, 10));
-// console.log(waitingTime("[6,2]", 9));
-// // console.log(waitingTime([6], 4));
-// console.log(waitingTime([7, 8, 3, 4, 5], "9"));
+console.log(waitingTime([13, 2], 6));
+console.log(waitingTime([13, 2, 6, 7, 10], 6));
+console.log(waitingTime([6], 4));
+console.log(waitingTime(7, 10));
+console.log(waitingTime("[6,2]", 9));
+console.log(waitingTime([7, 8, 3, 4, 5], "9"));
