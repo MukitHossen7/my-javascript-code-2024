ba5a9b1bac88fdba9718f2e23a3560c3cb53607c
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-11 22:30:25 +0600

    problem 2 is done

--- Milestone-4/B10-javascript-assignment-slove/problem2.js
+++ Milestone-4/B10-javascript-assignment-slove/problem2.js
@@ -6,13 +6,22 @@ function sendNotification(email) {
   let index = mail.indexOf("@");
   let userName = mail.slice(0, index).join("");
   let domainName = mail.slice(index + 1).join("");
+  if (!userName) {
+    return "Invalid Email";
+  } else if (!domainName) {
+    return "Invalid Email";
+  }
   let message = `${userName} sent you an email from ${domainName}`;
   return message;
 }
-console.log(sendNotification("zihad hossen @ gmail.com"));
-console.log(sendNotification("zihad@gmail.com"));
-console.log(sendNotification("farhan34@yahoo.com"));
-console.log(sendNotification("nadim.naem5@outlook.com"));
-console.log(sendNotification("fahim234.hotmail.com"));
-console.log(sendNotification("fahim234.hotmail.com"));
-console.log(sendNotification("sadia8icloud.com"));
+// console.log(sendNotification("zihad hossen @ gmail.com"));
+// console.log(sendNotification("zihad@gmail.com"));
+// console.log(sendNotification("farhan34@yahoo.com"));
+// console.log(sendNotification("nadim.naem5@outlook.com"));
+// console.log(sendNotification("fahim234.hotmail.com"));
+// console.log(sendNotification("fahim234.hotmail.com"));
+// console.log(sendNotification("sadia8icloud.com"));
+// console.log(sendNotification("sadia8icloud.com"));
+// console.log(sendNotification("@gmail.com"));
+// console.log(sendNotification("zihad"));
+console.log(sendNotification("zihad@@gmail.com"));
