a8fa6e35c0168031434afac467e260e84b9e6c04
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-11 08:40:36 +0600

    problem 3 is slove

---
+++ Milestone-4/B10-javascript-assignment-slove/problem3.js
@@ -0,0 +1,19 @@
+function checkDigitsInName(name) {
+  if (!(typeof name === "string")) {
+    return "Invalid Input";
+  }
+  let num = "0123456789";
+  for (let words of name) {
+    if (num.includes(words)) {
+      return true;
+    }
+  }
+  return false;
+}
+console.log(checkDigitsInName("Raj123"));
+console.log(checkDigitsInName("Suman"));
+console.log(checkDigitsInName("Name2024"));
+console.log(checkDigitsInName("!@#"));
+console.log(checkDigitsInName(["Raj"]));
+console.log(checkDigitsInName(12345));
+console.log(checkDigitsInName({ name: "mukit", age: 123 }));
