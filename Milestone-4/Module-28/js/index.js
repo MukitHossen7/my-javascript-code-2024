651577f2a31a6e524e349f1045a55c357116b93e
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-20 12:11:09 +0600

    Dynamic dom and data validation

--- Milestone-4/Module-28/js/index.js
+++ Milestone-4/Module-28/js/index.js
@@ -7,6 +7,10 @@ document
     const addAmountValue = getInputValueById("add-input-amount");
     const addPinValue = getInputValueById("add-input-pin");
     const amount = getTextValueById("amount");
+    if (isNaN(addAmountValue)) {
+      alert("Invalid Amount");
+      return;
+    }
     if (addPinValue === 1234) {
       let addAmount = amount + addAmountValue;
       document.getElementById("amount").innerText = addAmount;
@@ -26,14 +30,28 @@ document
     event.preventDefault();
     const cashOutAmountValue = getInputValueById("cashOut-input-amount");
     const cashOutPinValue = getInputValueById("cashOut-input-pin");
+    if (isNaN(cashOutAmountValue)) {
+      alert("Invalid Amount");
+      return;
+    }
     const amount = getTextValueById("amount");
+    if (cashOutAmountValue > amount) {
+      alert("Not enough balance!");
+      return;
+    }
     if (cashOutPinValue === 1234) {
       let cashOutAmount = amount - cashOutAmountValue;
       document.getElementById("amount").innerText = cashOutAmount;
 
-      let p = document.createElement("p");
-      p.innerText = `Cash Out: ${cashOutAmountValue} Tk. Total amount is ${cashOutAmount} Tk.`;
-      document.getElementById("history-section").appendChild(p);
+      let div = document.createElement("div");
+
+      div.innerHTML = `
+         <div class="bg-red-200 rounded-md p-2">
+                  <h2 class='text-xl font-bold'>Cash Out: ${cashOutAmountValue} TK</h2>
+                  <p class='font-medium'>Current Balance : ${cashOutAmount} TK</p>
+          </div>
+      `;
+      document.getElementById("history-section").appendChild(div);
     } else {
       alert("Invalid pin!");
     }
