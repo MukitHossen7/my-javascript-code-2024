8a555157208413d35239523645fc438eb2d1f323
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-25 23:17:37 +0600

    function default parameter

---
+++ Milestone-4/Module-30/class-30.2/default-parameter.js
@@ -0,0 +1,29 @@
+// function add(num1 = 0, num2 = 0) {
+//   const sum = num1 + num2;
+//   console.log(num1, num2, sum);
+// }
+// add(25, 30);
+// add(25);
+// add();
+
+// function myName(fname = "", lname = "") {
+//   const fullName = fname + " " + lname;
+//   console.log(fullName);
+// }
+// myName("Mukit", "Hossen");
+// myName("Mukit");
+// myName();
+
+// function myArray(arr = []) {
+//   console.log(arr);
+// }
+// myArray([1, 5, 10, 18, 50]);
+// myArray([]);
+// myArray();
+
+function myObj(obj = {}) {
+  console.log(obj);
+}
+myObj({ name: "Mukit", age: 25 });
+myObj({});
+myObj();
