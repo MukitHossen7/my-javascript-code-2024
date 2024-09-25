acb2b48987c6fdf3f60833b7e25d9b33ea0fe0d2
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-25 22:55:40 +0600

    let const practice done

---
+++ Milestone-4/Module-30/class-30.1/let-const.js
@@ -0,0 +1,29 @@
+// const k reassign kora jaba na
+// let k reassign kora jaba
+
+// Examples
+
+// const age = 25;
+// age = 50;
+// console.log(age);
+
+// let year = 2020;
+// year = 2024;
+// console.log(year);
+
+// let nums = [10, 50, 40, 80, 75];
+// nums = [50, 140, 150, 200];
+// console.log(nums);
+
+// const nums = [10, 50, 40, 80, 75];
+// nums[2] = 100;
+// nums[4] = 200;
+// console.log(nums);
+
+const person = {
+  name: "Mukit",
+  age: 25,
+};
+person.name = "Raju";
+person.age = 30;
+console.log(person);
