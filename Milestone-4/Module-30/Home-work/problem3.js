1638d8422593d86ddabe87687d5efc47eaac5220
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-26 16:59:36 +0600

    problem 3 is Done

---
+++ Milestone-4/Module-30/Home-work/problem3.js
@@ -0,0 +1,17 @@
+const squareNum = (squarearr) => {
+  const newSquareArray = [];
+  let sum = 0;
+
+  for (let square of squarearr) {
+    let squareValue = square ** 2;
+    newSquareArray.push(squareValue);
+  }
+  for (let sumSquareNum of newSquareArray) {
+    sum += sumSquareNum;
+  }
+  let count = newSquareArray.length;
+  let average = sum / count;
+  return average;
+};
+console.log(squareNum([2, 3, 4, 5, 6]));
+console.log(squareNum([1, 2, 5, 10, 4, 7]));
