cb55b0ac0013d2249eb210feba6c605eed79a8f7
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-27 00:01:22 +0600

    forEach and filter

---
+++ Milestone-4/Module-31/class-31.4/foreach-filter.js
@@ -0,0 +1,37 @@
+// forEach()
+// const numbers = [65, 44, 12, 4];
+// const print = numbers.forEach((n) => console.log(n));
+// const print = numbers.forEach((n) => {
+//   console.log(n);
+// });
+
+//filter()
+// const height = [50, 55, 60, 65, 75, 72, 62];
+// const heightNum = height.filter((num) => num > 70);
+// const heightNum2 = height.filter((num) => num > 60);
+// const heightNum3 = height.filter((num) => num > 50);
+// const heightNum4 = height.filter((num) => num > 80);
+// console.log(heightNum);
+// console.log(heightNum2);
+// console.log(heightNum3);
+// console.log(heightNum4);
+
+//find()
+// const height = [50, 55, 60, 65, 75, 72, 62];
+// const heightNum = height.find((num) => num > 50);
+// const heightNum2 = height.find((num) => num > 70);
+// const heightNum3 = height.find((num) => num > 80);
+// console.log(heightNum);
+// console.log(heightNum2);
+// console.log(heightNum3);
+
+//forEach();
+const numbers = [1, 2, 3, 4, 5];
+numbers.forEach((num) => {
+  console.log(num);
+});
+
+const fruits = ["apple", "banana", "mango"];
+fruits.forEach((fruits, index) => {
+  console.log(`${index} : ${fruits}`);
+});
