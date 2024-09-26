33374d166ad9c51f2690653597ff5ace1eed4f23
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-26 09:31:11 +0600

    more arrow function

---
+++ Milestone-4/Module-30/class-30.5/more-arrow.js
@@ -0,0 +1,19 @@
+// const add = (num1, num2) => num1 + num2;
+// console.log(add(10, 5));
+
+// const myObj = (person) => person.age;
+// console.log(myObj({ name: "Mukit", age: 25 }));
+
+// const myObj = (person) => person.age;
+// console.log(myObj({ name: "Mukit", age: 25 }));
+
+// const name = () => "Mukit hossen";
+// console.log(name());
+
+//large arrow function
+const morePara = (a, b, c, d) => {
+  const mult = a * b * c * d;
+  const sum = a + b + c + d;
+  return mult + sum;
+};
+console.log(morePara(4, 8, 5, 7));
