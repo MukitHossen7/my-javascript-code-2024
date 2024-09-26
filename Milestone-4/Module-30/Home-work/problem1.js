3d1483dbeea05f17873e74e093d4d50e8a68a4fc
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-26 15:53:03 +0600

    problem 1 is done

---
+++ Milestone-4/Module-30/Home-work/problem1.js
@@ -0,0 +1,16 @@
+//Task 1
+const multiply = (num1, num2, num3) => num1 * num2 * num3;
+console.log(multiply(5, 10, 5));
+
+//Task 2
+const message = `
+ I am a web developer.
+ I love to code.
+ I love to eat biryani.
+`;
+console.log(message);
+
+// Task 3
+
+const sum = (num1, num2 = 0) => num1 + num2;
+console.log(sum(10));
