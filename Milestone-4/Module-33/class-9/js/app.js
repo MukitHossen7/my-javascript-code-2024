2d80188c1aa50345a2eb1afa9c85af0ae490274e
MukitHossen7 <hossenmukit7@gmail.com>
2024-10-01 18:51:37 +0600

    all comment done

--- Milestone-4/Module-33/class-9/js/app.js
+++ Milestone-4/Module-33/class-9/js/app.js
@@ -1,16 +1,33 @@
-const todosFuction = () => {
-  fetch("https://jsonplaceholder.typicode.com/todos")
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.error("load error"));
-};
+// const todosFuction = () => {
+//   fetch("https://jsonplaceholder.typicode.com/todos")
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
+//     .catch((error) => console.error("load error"));
+// };
+
+// const todoFunction = async () => {
+//   try {
+//     const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+//     const data = await res.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.error("load error");
+//   }
+// };
 
 const todoFunction = async () => {
   try {
     const res = await fetch("https://jsonplaceholder.typicode.com/todos");
     const data = await res.json();
-    console.log(data);
+    const todoContainer = document.getElementById("todo-container");
+    data.map((item) => {
+      console.log(item);
+      const li = document.createElement("li");
+      li.innerHTML = item.title;
+      todoContainer.appendChild(li);
+    });
   } catch (error) {
     console.error("load error");
   }
 };
+todoFunction();
