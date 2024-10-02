c86779fa12382042c2caf8c18087226d2253c7bd
MukitHossen7 <hossenmukit7@gmail.com>
2024-10-02 09:04:57 +0600

    event loop

---
+++ Milestone-4/Module-34/class-34.6/event-loop.js
@@ -0,0 +1,37 @@
+function a() {
+  console.log("a");
+  b();
+  console.log("aa");
+}
+function b() {
+  console.log("b");
+  c();
+  console.log("bb");
+}
+function c() {
+  console.log("c");
+  console.log("cc");
+}
+
+function x() {
+  console.log("x");
+  y();
+  console.log("xx");
+}
+function y() {
+  console.log("y");
+  z();
+  console.log("yy");
+}
+function z() {
+  console.log("z");
+  console.log("zz");
+}
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+setTimeout(() => {
+  console.log("Set timer.....");
+}, 3000);
+a();
+x();
