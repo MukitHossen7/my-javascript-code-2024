0250ee4d0d389ab00613c8e0bebbac1ef06618ef
MukitHossen7 <hossenmukit7@gmail.com>
2024-10-17 00:02:36 +0600

    use effect is done

---
+++ Milestone-4/Module-39/my-useEffect-file/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Effects from "./components/Effects";
+
+function App() {
+  return (
+    <>
+      <h2>React Hocks- useEffect</h2>
+      <Effects></Effects>
+    </>
+  );
+}
+
+export default App;
