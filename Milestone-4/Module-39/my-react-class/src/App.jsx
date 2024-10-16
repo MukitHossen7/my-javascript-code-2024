592ada7ada10453fea0df8dbaeec8cca5787bcf2
MukitHossen7 <hossenmukit7@gmail.com>
2024-10-16 21:58:32 +0600

    use state is done

---
+++ Milestone-4/Module-39/my-react-class/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import State from "./components/State";
+
+function App() {
+  return (
+    <>
+      <h1>React State</h1>
+      <State></State>
+    </>
+  );
+}
+
+export default App;
