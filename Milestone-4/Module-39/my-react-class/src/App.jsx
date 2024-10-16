58e2b9017d07d17358e42e7c1dcdb49b3d6ee4e7
MukitHossen7 <hossenmukit7@gmail.com>
2024-10-16 23:44:17 +0600

    use state is done

--- Milestone-4/Module-39/my-react-class/src/App.jsx
+++ Milestone-4/Module-39/my-react-class/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import State from "./components/State";
+import Teams from "./components/Teams";
 
 function App() {
   return (
     <>
       <h1>React State</h1>
+      <Teams></Teams>
       <State></State>
     </>
   );
