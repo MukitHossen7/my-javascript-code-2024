dcd4dd052de8ac22a1e4eb3cbbb0a4d225d38594
MukitHossen7 <hossenmukit7@gmail.com>
2024-10-17 10:38:12 +0600

    show details done

--- Milestone-4/Module-39/my-useEffect-file/src/App.jsx
+++ Milestone-4/Module-39/my-useEffect-file/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Details from "./components/Details";
 import Effects from "./components/Effects";
 
 function App() {
   return (
     <>
       <h2>React Hocks- useEffect</h2>
+      <Details></Details>
       <Effects></Effects>
     </>
   );
